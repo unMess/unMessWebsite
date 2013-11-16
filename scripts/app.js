c19ceeb9b4d4399b6f97b02a9af63974600a6bd9
Thomas Smith <tom.smith@imulus.com>
2013-11-16 13:11:47 -0700

    Add background cycle functions

--- scripts/app.js
+++ scripts/app.js
@@ -6,5 +6,19 @@ $( function() {
     $('.bg').height(winY);
 
     // fade in first bg on load
-    $('.bg1').fadeIn(3000);
+    //$('.bg > div').each( function(index) {
+
+    var $backgrounds = $('.bg > div');
+    var i = 0;
+
+    ( function cycleBackgrounds() {
+
+        $backgrounds.eq(i).fadeIn(2000)
+            .delay(5000)
+            .fadeOut(2000, cycleBackgrounds);
+
+        i = ++i % $backgrounds.length;
+    })();
+        
+
 }); 
