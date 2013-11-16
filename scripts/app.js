cce64df21a172df0854d3b57632e3f431bf00d3a
Thomas Smith <tom.smith@imulus.com>
2013-11-16 15:33:07 -0700

    Wire up the buttons

--- scripts/app.js
+++ scripts/app.js
@@ -9,16 +9,34 @@ $( function() {
     //$('.bg > div').each( function(index) {
 
     var $backgrounds = $('.bg > div');
+    var $buttons = $('.sliderButton');
     var i = 0;
 
-    ( function cycleBackgrounds() {
+    var cycle = ( function cycleBackgrounds() {
+        var $thisBg = $backgrounds.eq(i);
 
-        $backgrounds.eq(i).fadeIn(2000)
-            .delay(5000)
-            .fadeOut(2000, cycleBackgrounds);
+        $buttons.removeClass('active');
+        $thisBg.fadeIn(2000);
+
+        setTimeout( function(){
+            $thisBg.fadeOut(2000, cycleBackgrounds);
+        }, 5000);
+
+        $buttons.eq(i).addClass('active');
 
         i = ++i % $backgrounds.length;
+
+        return function(){};
     })();
         
+    $buttons.on('click', function() {
+        var index = $(this).index();
+        i = index;
+
+        $buttons.eq(i).addClass('active');
+        $backgrounds.fadeOut(2000, function(){
+            cycle()
+        });
+    });
 
 }); 
