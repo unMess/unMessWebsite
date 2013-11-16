fcfe9e5e61a1c4c14da8314710c097cf807100a4
Thomas Smith <tom.smith@imulus.com>
2013-11-16 12:28:13 -0700

    Fade in first bg on load

--- scripts/app.js
+++ scripts/app.js
@@ -2,14 +2,9 @@ $( function() {
     var winX = $(window).width();
     var winY = $(window).height();
 
-    console.log(winX);
-    console.log(winY);
-
-    // $('.bg').each( function() {
-    //     $(this).fadeIn().fadeOut();
-    // });
     $('.bg').width(winX);
     $('.bg').height(winY);
 
-//    $('.bg1').fadeIn().delay(2000).next('.bg').fadeIn();
+    // fade in first bg on load
+    $('.bg1').fadeIn(3000);
 }); 
